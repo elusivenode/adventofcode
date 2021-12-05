d5a89e53073bd9b6f293b8f723507716be035084
Hamish MacDonald <hamish.macdonald@servian.com>
2021-12-05 21:36:30 +1000

    Day 2 part 1

---
+++ cmd/2021_2/day2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	file, err := os.Open("/Users/hamishmacdonald/Learning/go/adventofcode/assets/input_day2")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	var hori, depth int
+	for _, l := range lines {
+		direction, length := getDirections(l)
+		if direction == "up" {
+			depth -= length
+		} else if direction == "down" {
+			depth += length
+		} else if direction == "back" {
+			hori -= length
+		} else if direction == "forward" {
+			hori += length
+		} else {
+			fmt.Println("Unknown direction.")
+		}
+	}
+	fmt.Printf("Depth is %v and horizonal is %v\n", depth, hori)
+	fmt.Printf("Answer is %v\n", depth * hori)
+}
+
+func getDirections(dirs string) (string, int) {
+	dirsParse := strings.Fields(dirs)
+	direction := dirsParse[0]
+	length, _ :=  strconv.Atoi(dirsParse[1])
+	return direction, length
+}
