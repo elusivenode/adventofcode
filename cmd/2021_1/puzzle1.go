efe2c8a7b7f6359210334d91f53f3c48037e3df1
Hamish MacDonald <hamish.macdonald@servian.com>
2021-12-05 21:12:54 +1000

    Day 1 part 2

--- cmd/2021_1/puzzle1.go
+++ cmd/2021_1/puzzle1.go
@@ -22,13 +22,33 @@ func main() {
 		lines = append(lines, scanner.Text())
 	}
 
-	var curr, prev, ctr int
+	var curr, prev, ctr, sumCurr3, sumPrev3, ctr3 int
+	var curr3, prev3 []string
 	for i,s := range lines {
 		prev = curr
 		curr, err = strconv.Atoi(s)
 		if i > 0 && curr > prev {
 			ctr++
 		}
+		if i > 2 {
+			prev3 = lines[i-3:i]
+			curr3 = lines[i-2:i+1]
+			sumPrev3 = sumSlice(prev3)
+			sumCurr3 = sumSlice(curr3)
+			if sumCurr3 > sumPrev3 {
+				ctr3++
+			}
+		}
 	}
-	fmt.Printf("Number of measurements greater than their predecessor is %v", ctr)
+	fmt.Printf("Number of measurements greater than their predecessor is %v\n", ctr)
+	fmt.Printf("Number of sliding 3 block windows greater than their predecessor is %v", ctr3)
+}
+
+func sumSlice(subslice []string) int {
+	var v, sum int
+	for _,s := range subslice {
+		v,_ = strconv.Atoi(s)
+		sum += v
+	}
+	return sum
 }
