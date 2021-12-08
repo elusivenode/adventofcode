8019f2edd70edeeadea1a8bff18f5f757ff3fa91
Hamish MacDonald <hamish.macdonald@servian.com>
2021-12-08 20:36:41 +1000

    Day 3 part 2

---
+++ cmd/2021_3_2/day3_2.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	file, err := os.Open("/Users/hamishmacdonald/Learning/go/adventofcode/assets/input_day3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	oxygen := make([]string, len(lines))
+	copy(oxygen, lines)
+	co2 := make([]string, len(lines))
+	copy(co2, lines)
+
+	var foundOxygen, foundCO2 bool
+	foundOxygen = false
+	foundCO2 = false
+
+	i := 0
+	for !foundOxygen {
+		mostCommon, _ := getCounts(oxygen, i)
+		oxygen = findElement(oxygen, mostCommon, i)
+		if len(oxygen) == 1 {
+			foundOxygen = true
+		}
+		i++
+	}
+
+	i = 0
+	for !foundCO2 {
+		_, leastCommon := getCounts(co2, i)
+		co2 = findElement(co2, leastCommon, i)
+		if len(co2) == 1 {
+			foundCO2 = true
+		}
+		i++
+	}
+	oxygenRating, _ := strconv.ParseInt(oxygen[0], 2, 64)
+	co2ScrubberRating, _ := strconv.ParseInt(co2[0], 2, 64)
+	fmt.Printf("Oxygen rating is: %v; CO2 scrubber rating: %v; Life support rating is: %v", oxygenRating, co2ScrubberRating, oxygenRating * co2ScrubberRating)
+}
+
+func getCounts(slice []string, idx int) (mostCommon uint8, leastCommon uint8) {
+	zeros := 0
+	ones := 0
+	for _, x := range slice {
+		if x[idx] == '0' {
+			zeros++
+		} else {
+			ones++
+		}
+	}
+	if zeros > ones {
+		mostCommon = '0'
+		leastCommon = '1'
+	} else {
+		leastCommon = '0'
+		mostCommon = '1'
+	}
+	return mostCommon, leastCommon
+}
+
+func findElement(slice []string, criteria uint8, idx int) []string{
+	i := 0
+	for _, x := range slice {
+		if x[idx] == criteria  {
+			slice[i] = x
+			i++
+		}
+	}
+	return slice[:i]
+}
